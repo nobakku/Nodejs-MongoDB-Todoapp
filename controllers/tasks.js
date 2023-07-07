a332b859fed87e4f5e1ce1e7465a7058ee54c0ef
nobata <ahirururu122@gmail.com>
2023-07-08 08:10:29 +0900

    APIの作成

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,70 @@
+const Task = require("../models/Task");
+
 // タスクの中身を記述
-const getAllTasks = (req, res) => {
-  res.send("タスクを全て取得しました");
+
+// 全てのタスクを取得
+const getAllTasks = async (req, res) => {
+  try {
+    const allTask = await Task.find({});
+    res.status(200).json(allTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
-const createTask = (req, res) => {
-  res.send("タスクを新規作成しました");
+
+// タスクを新規作成
+const createTask = async (req, res) => {
+  try {
+    const createTask = await Task.create(req.body);
+    res.status(200).json(createTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
-const getSingleTask = (req, res) => {
-  res.send("ある特定のタスクを取得しました");
+
+// ある特定のタスクを取得
+const getSingleTask = async (req, res) => {
+  try {
+    const getSingleTask = await Task.findOne({ _id: req.params.id });
+    if (!getSingleTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(getSingleTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
-const updateTask = (req, res) => {
-  res.send("ある特定のタスクを更新しました");
+
+// ある特定のタスクを更新
+const updateTask = async (req, res) => {
+  try {
+    const updateTask = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    if (!updateTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(updateTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
-const deleteTask = (req, res) => {
-  res.send("ある特定のタスクを削除しました");
+
+// ある特定のタスクを削除
+const deleteTask = async (req, res) => {
+  try {
+    const deleteTask = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!deleteTask) {
+      return res.status(404).json(`_id: ${req.params.id}は存在しません`);
+    }
+    res.status(200).json(deleteTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
